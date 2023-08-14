056e42cb3616562f5d81d1fb837c07c5220859c3
Ehasan <mdehasan1053@gmail.com>
2023-08-15 00:30:17 +0530

    update app.js

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ passport.use(
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
       callbackURL: process.env.AUTH_URL,
-      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+      // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     function (accessToken, refreshToken, profile, cb) {
       // console.log(profile);
